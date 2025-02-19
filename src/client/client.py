f787209763e2750fb0cc87289c6b486e6b0394bf
PavelLinearB <pavel.vaks@linearb.io>
2025-02-19 17:38:51 +0200

    fix cr

--- src/client/client.py
+++ src/client/client.py
@@ -30,14 +30,27 @@ try:
         sock.connect((host, port))
         sock.sendall(str.encode(name))
 except (socket.error, ConnectionRefusedError) as e:
+        # Create new thread to wait for data
+        stop_event = threading.Event()
+        receiveThread = threading.Thread(target=receive, args=(sock, stop_event))
+        receiveThread.start()
+        # Send data to server
+        try:
+            while True:
+                message = input()
+                if not message:  # Allow clean exit on empty input
+                    break
+                sock.sendall(str.encode(message))
+        except (socket.error, ConnectionResetError) as e:    
+            print(f"Connection error: {e}")
+        finally:
+            print("Closing connection...")
+            stop_event.set()  # Signal receive thread to stop
+            receiveThread.join()
+except (socket.error, ConnectionRefusedError) as e:
     print("Could not make a connection to the server. Error: " + str(e))
     input("Press enter to quit")
     sys.exit(0)
-
-# Create new thread to wait for data
-stop_event = threading.Event()
-receiveThread = threading.Thread(target=receive, args=(sock, stop_event))
-receiveThread.start()
 
 # Send data to server
 # str.encode is used to turn the string message into bytes so it can be sent across the network
