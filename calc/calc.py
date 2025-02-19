3313a7ebf84e2cadd41e08006b05999ef91f6172
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2025-02-20 00:18:53 +0200

    Update calc.py

--- calc/calc.py
+++ calc/calc.py
@@ -28,26 +28,30 @@ operations = {
     '3': (multiply, '*'),
     '4': (divide, '/')
 }
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        func, operator = operations[choice]
-        print(num1, operator, num2, "=", func(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation.lower() == "no":
-            break
-    else:
-        print("Invalid Input")
+def main():
+    while True:
+        # take input from the user
+        choice = input("Enter choice(1/2/3/4): ")
+    
+        # check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+                continue
+    
+            func, operator = operations[choice]
+            print(num1, operator, num2, "=", func(num1, num2))
+            
+            # check if user wants another calculation
+            # break the while loop if answer is no
+            next_calculation = input("Let's do next calculation? (yes/no): ")
+            if next_calculation.lower() == "no":
+                break
+        else:
+            print("Invalid Input")
+            
+if __name__ == "__main__":
+    main()
