f65ea1f4f3d5f1ce89645d521e86767a6efb4564
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2025-02-20 12:16:13 +0200

    Update calc.py

--- calc/calc.py
+++ calc/calc.py
@@ -16,12 +16,6 @@ def divide(x, y):
         raise ValueError("Cannot divide by zero")
     return x / y
 
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
 operations = {
     '1': (add, '+'),
     '2': (subtract, '-'),
@@ -29,6 +23,12 @@ operations = {
     '4': (divide, '/')
 }
 def main():
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+    
     while True:
         # take input from the user
         choice = input("Enter choice(1/2/3/4): ")
@@ -45,10 +45,13 @@ def main():
             func, operator = operations[choice]
             print(num1, operator, num2, "=", func(num1, num2))
             
-            # check if user wants another calculation
+            # check if the user wants another calculation
             # break the while loop if answer is no
-            next_calculation = input("Let's do next calculation? (yes/no): ")
-            if next_calculation.lower() == "no":
+            while True:
+                next_calculation = input("Let's do next calculation? (yes/no): ").lower()
+                if next_calculation in ["yes", "no"]:
+                    break
+            if next_calculation == "no":
                 break
         else:
             print("Invalid Input")
